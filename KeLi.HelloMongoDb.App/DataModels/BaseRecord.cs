39a7d96afff47f78e2470939c7e10ae0c35e3703
KeLi <kelicto@protonmail.com>
2021-05-17 00:00:56 +0800

    Updates code.

---
+++ KeLi.HelloMongoDb.App/DataModels/BaseRecord.cs
@@ -0,0 +1,9 @@
+﻿namespace KeLi.HelloMongoDb.App.DataModels
+{
+    public abstract class BaseRecord
+    {
+        public string Name { get; set; }
+
+        public string Remark { get; set; }
+    }
+}
---
+++ KeLi.HelloMongoDb.App/DataModels/Student.cs
@@ -0,0 +1,9 @@
+﻿namespace KeLi.HelloMongoDb.App.DataModels
+{
+    public class Student : BaseRecord
+    {
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
